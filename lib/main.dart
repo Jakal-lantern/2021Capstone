8f96cab3c24ccbe34061c98e55b207b4af22b425
Alex Sarene <sarene.alex@gmail.com>
2021-04-29 13:33:48 -0400

    Created event template

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,100 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.deepPurpleAccent,
         ),
         body: SafeArea(
-          child: Column(    // TODO: Create event container template
+          child: Column(
             verticalDirection: VerticalDirection.down,    // Default
             crossAxisAlignment: CrossAxisAlignment.stretch,   // Stretch children across screen width
             children: <Widget>[
+              // First version of container template
               Container(
                 padding: EdgeInsets.all(10.0),
-                height: 100.0,
+                height: 125.0,
                 color: Colors.white,
+                child: Row(
+                  children: [
+                    // NAME AND DESCRIPTION
+                    Flexible(
+                      child: FractionallySizedBox(
+                        widthFactor: 1,   // This seems to be working
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+                                child: Text("Sick Name",
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w800
+                                  )
+                                )
+                            ),
+                            Text("This is the description of an event. Kinda cool right? You can really have whatever you want! I should implement a char limit though.")
+                          ],
+                        )
+                      ),
+                    ),
+                    // TIME AND DATE
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          left: BorderSide(
+                            width: 1.0,
+                          )
+                        )
+                      ),
+                      width: 75.0,    // TEMP: I want as little hard-coded values as possible
+                      margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(   // Probably a better way to write this
+                            children: [
+                              Text('3:00',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold
+                                )
+                              ),
+                              Text('pm',
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold
+                                )
+                              )
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text('January',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  )
+                              ),
+                              Text('4th',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  )
+                              ),
+                              Text('2021',
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  )
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )
+
+
+
+                /*
                 child: Row(
                   children: [
                     Column(
@@ -43,6 +129,7 @@ class MyApp extends StatelessWidget {
                     )
                   ],
                 ),
+                */
               ),
               Container(
                 height: 100.0,
@@ -113,7 +200,7 @@ class MyApp extends StatelessWidget {
               )
             ],
           ),
-          elevation: 0,   // Do I need this?
+          elevation: 0,
         )
       )
     );
